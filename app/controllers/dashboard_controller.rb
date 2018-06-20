abd446721be0215f895f06c0563714eecb1be6dc
Nathaniel Miller <nathaniel@m.ller.io>
2018-06-19 17:02:46 -0700

    Moved the invitations table into partial. Partial rendered in invitations index and user hashboard. Added query to dashboard controller to only select invitations that matched the logged in user's email address.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,8 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @user = current_user
+    @circles = @user.circles
+    @invitations = Invitation.where("email = ?", @user.email)
   end
 end

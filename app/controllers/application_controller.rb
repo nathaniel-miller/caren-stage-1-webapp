936508bdc15e0d5e7b02594deb532d13e403875d
Nathaniel Miller <nathaniel@m.ller.io>
2018-06-21 16:46:53 -0700

    Add user authorization.
    Logged in user can only interact with resources within circles that they have a position in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :authorize_user
+
+  def authorize_user
+    if params[:controller] == "circles"
+      circle_id = params[:id]
+    elsif params[:circle_id]
+      circle_id = params[:circle_id]
+    end
+    
+    if circle_id && !current_user.positions.find_by(circle_id: circle_id)
+      redirect_to root_path
+    end
+  end
 end

2ed5b7b81e918309717e26a82de93505c5114608
Nathaniel Miller <nathaniel@m.ller.io>
2018-06-20 18:12:43 -0700

    Nested tasks, posts, info, task gens, and positions under circle.

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,13 @@ Rails.application.routes.draw do
   resources :categories
   resources :roles
   resources :invitations
-  resources :positions, only: [:destroy]
 
   resources :circles, except: [:index] do
     resources :tasks
     resources :posts
     resources :important_info_pieces
     resources :task_generators
-    resources :positions, except: [:destroy]
+    resources :positions
   end
   
   get 'dashboard/index'
